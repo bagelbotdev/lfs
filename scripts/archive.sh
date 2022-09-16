52327d2a9e3577741b817cb4540e3d568454e91f
erwijet <tsh6656@rit.edu>
2022-09-15 22:46:55 -0400

    feat(scripts/archive): support new lfs binaries

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -16,14 +16,18 @@ git reset --hard origin/master
 git pull origin master
 
 (cd $data_path && tar cf - *) |
-	gzip -9 > $SCRIPT_DIR/../$archive_name
+	gzip -9 > $SCRIPT_DIR/../$archive_name.tgz
 
 cd $SCRIPT_DIR/..
-gpg -c $SCRIPT_DIR/../$archive_name
-rm $SCRIPT_DIR/../$archive_name
+gpg -c $SCRIPT_DIR/../$archive_name.tgz
+rm $SCRIPT_DIR/../$archive_name.tgz
 
 git config user.name "bagelbot-lfs"
 git config user.email "bagelbot@erwijet.com"
 
-git commit -a -m "$(date)"
+git lfs track $archive_name.tgz.gpg
+git add $archive_name.tgz.gpg
+git add .gitattributes
+
+git commit -m "$(date)"
 git push origin master
